ddefbbce91f6a957b56d8aae728fd6a25dc42281
LaurisHebert <lrshebert@gmail.com>
2023-09-21 12:14:25 +0200

    Optimisation

--- src/main/java/com/simplon/dvd/controllers/ClientController.java
+++ src/main/java/com/simplon/dvd/controllers/ClientController.java
@@ -1,6 +1,7 @@
 package com.simplon.dvd.controllers;
 
 import com.simplon.dvd.DTO.ClientDTO;
+import com.simplon.dvd.DTO.ClientGetDTO;
 import com.simplon.dvd.services.ClientService;
 import com.simplon.dvd.services.ClientServiceModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,44 +17,33 @@ public class ClientController {
     ClientService service;
 
     @PostMapping
-    public void save(@RequestBody ClientDTO client){
+    public void save(@RequestBody ClientDTO client) {
         service.save(new ClientServiceModel(client));
     }
+
     @GetMapping("/{id}")
-    public ClientDTO findById (@PathVariable long id){
+    public ClientGetDTO findById(@PathVariable long id) {
         ClientServiceModel client = service.findById(id);
-        return new ClientDTO(
-                client.getClientNumber(),
-                client.getFirstName(),
-                client.getLastName(),
-                client.getEmail(),
-                client.getRegistrationDate()
-        );
+        return new ClientGetDTO(client);
     }
 
     @GetMapping
-    public List<ClientDTO> findAll(){
-        List<ClientDTO> clients = new ArrayList<>();
+    public List<ClientGetDTO> findAll() {
+        List<ClientGetDTO> clients = new ArrayList<>();
 
-        for (ClientServiceModel client : service.findAll() ) {
-            clients.add(new ClientDTO(
-                    client.getClientNumber(),
-                    client.getFirstName(),
-                    client.getLastName(),
-                    client.getEmail(),
-                    client.getRegistrationDate()
-            ));
+        for (ClientServiceModel client : service.findAll()) {
+            clients.add(new ClientGetDTO(client));
         }
         return clients;
     }
 
-    @PutMapping ("{id}")
-    public void update (@PathVariable long id, @RequestBody ClientDTO client){
+    @PutMapping("{id}")
+    public void update(@PathVariable long id, @RequestBody ClientDTO client) {
         service.save(new ClientServiceModel(id, client));
     }
 
     @DeleteMapping("{id}")
-    public void deleteById (@PathVariable long id){
+    public void deleteById(@PathVariable long id) {
         service.deleteById(id);
     }
 }
