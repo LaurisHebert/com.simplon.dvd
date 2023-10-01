f00363e11aab9ade071673c5076c19fb1dd6aaa5
LaurisHebert <lrshebert@gmail.com>
2023-10-01 14:52:33 +0200

    test new gitIgnor

--- Back-end/src/main/java/com/simplon/dvdStore/BackEndApplication.java
+++ Back-end/src/main/java/com/simplon/dvdStore/BackEndApplication.java
@@ -2,10 +2,22 @@ package com.simplon.dvdStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEndApplication {
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
 
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+    }
     public static void main(String[] args) {
         SpringApplication.run(BackEndApplication.class, args);
     }
